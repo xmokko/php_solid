6754c00cd938301c2f76bdfa764673380a83f12f
xmokko <solloz.mail@gmail.com>
2021-01-24 15:54:22 +0200

    First commit

--- PersonDB.php
+++ PersonDB.php
@@ -2,13 +2,15 @@
 
 namespace Person;
 
+use PDO;
+
 require_once 'IPerson.php';
 
 class PersonDB implements IPerson
 {
     public function save($file_name, $person_name)
     {
-        $db = new \PDO('sqlite:' . $file_name);
+        $db = new PDO('sqlite:' . $file_name);
         $db->query("CREATE TABLE IF NOT EXISTS test ( person_name varchar(40) DEFAULT 0);");
         $db->query("INSERT into test VALUES ('$person_name')");
     }
